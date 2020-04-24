21cbd109e5fc321b88536b3991aea2b2fdbcf68a
LyhourChhen <ukamilove66@gmail.com>
2020-04-24 16:41:52 +0700

    successfully get data

---
+++ scraping.js
@@ -0,0 +1,23 @@
+const request = require("request");
+const cheerio = require("cheerio");
+var words = require("voca/words");
+request(
+  "https://www.worldometers.info/coronavirus/country/cambodia/",
+  (err, res, html) => {
+    if (!err && res.statusCode == 200) {
+      const $ = cheerio.load(html);
+
+      const data = $(".maincounter-number");
+      const arrayData = words(data.text());
+      console.log(arrayData);
+
+      let newData = [
+        { cases: arrayData[0] },
+        { deaths: arrayData[1] },
+        { recoverd: arrayData[2] },
+      ];
+
+      console.log(JSON.stringify(newData));
+    }
+  }
+);
