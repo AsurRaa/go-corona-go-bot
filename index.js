2383e16d8a46d04fbd568fe4155878609d9372af
LyhourChhen <ukamilove66@gmail.com>
2020-04-24 17:11:31 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+"use strict";
+
 require("dotenv").config();
 const color = require("colors");
 const Telegraf = require("telegraf");
+const scraping = require("./scraping");
+
+const message = `ព័ត៌មានផ្លូវការ អំពី Covid-19 នៅកម្ពុជា 
+
+គិតត្រឹម "ថ្ងៃទី២៣ មេសា ២០២០"
+- ចំនួនអ្នកឆ្លង៖ " ១២២ នាក់ "
+- ចំនួនអ្នកជាសះស្បើយ៖ " ១១០ នាក់ "
+- ចំនួនអ្នកស្លាប់៖ គ្មាន
+`;
+
+// run scraping
+scraping();
+console.log(color.red("Scaping Script is running !!!"));
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.command("covid", (ctx) => ctx.reply("hi from covid"));
+bot.hears("corona", (ctx) => ctx.reply(message));
 bot.launch();
--- scraping.js
+++ scraping.js
@@ -1,23 +1,30 @@
+"use strict";
+
 const request = require("request");
 const cheerio = require("cheerio");
 let words = require("voca/words");
-request(
-  "https://www.worldometers.info/coronavirus/country/cambodia/",
-  (err, res, html) => {
-    if (!err && res.statusCode == 200) {
-      const $ = cheerio.load(html);
 
-      const data = $(".maincounter-number");
-      const arrayData = words(data.text());
-      console.log(arrayData);
+const scraping = () => {
+  return request(
+    "https://www.worldometers.info/coronavirus/country/cambodia/",
+    (err, res, html) => {
+      if (!err && res.statusCode == 200) {
+        const $ = cheerio.load(html);
 
-      let newData = [
-        { cases: arrayData[0] },
-        { deaths: arrayData[1] },
-        { recoverd: arrayData[2] },
-      ];
+        const data = $(".maincounter-number");
+        const arrayData = words(data.text());
+        console.log(arrayData);
 
-      console.log(JSON.stringify(newData));
+        let newData = [
+          { cases: arrayData[0] },
+          { deaths: arrayData[1] },
+          { recoverd: arrayData[2] },
+        ];
+
+        console.log(JSON.stringify(newData));
+      }
     }
-  }
-);
+  );
+};
+
+exports.default = scraping;
