56a338180193d6b7e662b67a8c3b0b7bc0d12210
LyhourChhen <ukamilove66@gmail.com>
2020-04-26 09:19:34 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 import Telegraf from "telegraf";
-import scraping from "./scraping";
+
 import voca from "voca";
+import cheerio from "cheerio";
+import words from "voca/words";
 import moment, { locale } from "moment";
 require("dotenv").config();
+const request = require("request");
 
 const today = moment(new Date(), locale("km")).format("ll");
 const todayWithDay = moment(new Date(), locale("km")).format("LLLL");
 const day = voca(todayWithDay).chain().lowerCase().words().value();
+const country = "cambodia";
 
+let data = [];
+
+request(
+  `https://www.worldometers.info/coronavirus/country/${country}/`,
+  (err, res, html) => {
+    if (!err && res.statusCode == 200) {
+      const $ = cheerio.load(html);
+
+      let hook = $(".maincounter-number");
+      const arrayData = words(hook.text());
+      console.log(arrayData);
+      data.push({
+        cases: arrayData[0],
+        die: arrayData[1],
+        recovered: arrayData[2],
+      });
+    }
+  }
+);
+
+console.log("hwhwhwh", data);
 const message = `ព័ត៌មានផ្លូវការ អំពី Covid-19 នៅកម្ពុជា 
 គិតត្រឹម "ថ្ងែ${day[0]}, ${today}"
 - ចំនួនអ្នកឆ្លង៖ " ១២២ នាក់ "
@@ -15,15 +40,15 @@ const message = `ព័ត៌មានផ្លូវការ អំពី Covi
 - ចំនួនអ្នកស្លាប់៖ គ្មាន
 `;
 
-// run scraping
-scraping("cambodia");
-console.log("console scraping", scraping());
+// const main = async () => {
+//   const bot = new Telegraf(process.env.BOT_TOKEN);
+//   bot.start((ctx) => ctx.reply("Welcome"));
+//   bot.help((ctx) => ctx.reply("Send me a sticker"));
+//   bot.on("sticker", (ctx) => ctx.reply("👍"));
+//   bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+//   bot.command("covid", (ctx) => ctx.reply("hi from covid"));
+//   bot.hears("corona", (ctx) => ctx.reply(message));
+//   bot.launch();
+// };
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-bot.command("covid", (ctx) => ctx.reply("hi from covid"));
-bot.hears("corona", (ctx) => ctx.reply(message));
-bot.launch();
+// main();
--- scraping.js
+++ scraping.js
@@ -4,7 +4,7 @@ import cheerio from "cheerio";
 import words from "voca/words";
 
 const scraping = async (country) => {
-  request(
+  const getScraping = request(
     `https://www.worldometers.info/coronavirus/country/${country}/`,
     (err, res, html) => {
       if (!err && res.statusCode == 200) {
@@ -13,15 +13,10 @@ const scraping = async (country) => {
         let hook = $(".maincounter-number");
         const arrayData = words(hook.text());
 
-        // data[0] = { cases: arrayData[0] };
-        // data[1] = { deaths: arrayData[1] };
-        // data[2] = { recoverd: arrayData[2] };
-        console.log("arrayData", arrayData);
-
-        return arrayData;
       }
     }
   );
-};
+  
+
 
 export default scraping;
