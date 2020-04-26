d567872c7cebb7b7bb8cf2e0cd2c4c8a8fd207b7
LyhourChhen <ukamilove66@gmail.com>
2020-04-26 10:21:21 +0700

    update promise

--- scraping.js
+++ scraping.js
@@ -4,24 +4,26 @@ import cheerio from "cheerio";
 import words from "voca/words";
 
 const scarping = async () => {
-  let data = await [];
+  let data = [];
   await axios
     .get("https://www.worldometers.info/coronavirus/country/cambodia/")
     .then((res) => {
-      // console.log(res.data);
       const html = res.data;
       const $ = cheerio.load(html);
       let hook = $(".maincounter-number");
       const arrayData = words(hook.text());
-      data.push({
+      return data.push({
         cases: arrayData[0],
         dies: arrayData[1],
         recovered: arrayData[2],
       });
     })
     .catch((err) => console.log("render err", err));
-  console.log("data", data);
   return data;
 };
 
-console.log(scarping());
+async function run() {
+  const result = await scarping();
+  return result;
+}
+export default run;
